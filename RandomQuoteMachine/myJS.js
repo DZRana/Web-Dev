3a0dee64e6767c778ee51173293475794f52e615
DZRana <danzelrana@gmail.com>
2016-08-08 00:53:15 -0700

    api calls and styling done

---
+++ RandomQuoteMachine/myJS.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	var html = "";
+	var randomHex = '#'+ Math.floor(Math.random()*16777215).toString(16);
+	$.ajax ({
+    	url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous', // The URL to the API. You can get this in the API page of the API you intend to consume
+    	type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+    	data: {}, // Additional parameters here
+    	dataType: 'json',
+    	success: function(data) { 
+    		console.dir((data.source));	
+			$("body").css("color", randomHex);
+			$("body").css("background-color", randomHex);
+			$(".target").css("background-color", randomHex);
+			$(".btn-twitter").css("background-color", randomHex);
+    		html += "<h1><b>\"</b>" + data.quote + "</h1>";
+    		html += "<h2>-" + data.author + "</h2>";
+    		$("#currentQuote").html(html);
+    	},
+    	error: function(err) { alert(err); },
+    	beforeSend: function(xhr) {
+    		xhr.setRequestHeader("X-Mashape-Authorization", "FW3XLF2OmimshJmZljc3nqWJ9ojxp1w7yzdjsnr41vaFKR0BwF"); // Enter here your Mashape key
+    	}
+	});
+	$(".target").click(function() {
+		html = "";
+		$.ajax ({
+    		url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
+    		type: 'GET',
+    		data: {},
+    		dataType: 'json',
+    		success: function(data) { 
+    			console.dir((data.source));
+    			randomHex = '#'+ Math.floor(Math.random()*16777215).toString(16);
+				$("body").css("color", randomHex);
+				$("body").css("background-color", randomHex);
+				$(".target").css("background-color", randomHex);
+				$(".btn-twitter").css("background-color", randomHex);
+    			html += "<h1><b>\"</b>" + data.quote + "</h1>";
+    			html += "<h2>-" + data.author + "</h2>";
+    			$("#currentQuote").html(html);  			
+    		},
+    		error: function(err) { alert(err); },
+    		beforeSend: function(xhr) {
+    			xhr.setRequestHeader("X-Mashape-Authorization", "FW3XLF2OmimshJmZljc3nqWJ9ojxp1w7yzdjsnr41vaFKR0BwF");
+    		}
+		});
+	});
+});
