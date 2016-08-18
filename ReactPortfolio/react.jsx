12b67b32b42ebdd43394f202f1786ae5e37c66d0
DZRana <danzelrana@gmail.com>
2016-08-17 19:04:57 -0700

    starting react implementation

--- ReactPortfolio/react.jsx
+++ ReactPortfolio/react.jsx
@@ -1,16 +1,43 @@
-var Movie = React.createClass ({
+var Header = React.createClass({
 	render: function() {
 		return (
-			<div>
-				<h1>{this.props.title}</h1>
-				<h2>{this.props.genre}</h2>
+			<header>
+				<div className="row">
+					<div className="col-md-6">
+						<h1>Hey, there! My name is Danzel and I'm a software
+						engineer and aspiring web developer. I graduated from California
+						State Polytechnic University, Pomona with a Bachelor's degree in Computer Science
+						and am focusing my career on Full Stack Web Development.</h1>
+						<h2>{this.props.titles}</h2>
+					</div>
+					<div className="col-md-6">
+						<h1>TEST</h1>
+					</div>
+				</div>			
+			</header>
+		);
+	}
+});
+
+var Main = React.createClass({
+	render: function() {
+		return (
+			<main>
+				<h3>test main</h3>
+			</main>
+		);
+	}
+});
+
+var Content = React.createClass({
+	render: function() {
+		return (
+			<div>			
+				<Header titles="Web Developer - Software Engineer"/>
+				<Main />
 			</div>
 		);
 	}
 });
 
-ReactDOM.render(<div>
-					<Movie title="Avatar" genre="Action"/>
-					<Movie title="The Notebook" genre="Romance"/>
-					<Movie title="Star Wars" genre="Action"/>
-				</div>, document.getElementById("example"));
+ReactDOM.render(<Content />, document.getElementById("content-container"));
