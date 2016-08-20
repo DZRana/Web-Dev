23a0d7940193727604c555c45efdd4bdd3ecb106
DZRana <danzelrana@gmail.com>
2016-08-19 22:19:48 -0700

    continuing react implementation

--- ReactPortfolio/react.jsx
+++ ReactPortfolio/react.jsx
@@ -1,19 +1,45 @@
+var Nav = React.createClass({
+	render: function() {
+		return (
+			<nav className="navbar navbar-inverse navbar-fixed-top">
+        		<div className="navbar-header">
+          			<button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            			<span className="icon-bar"></span> 
+            			<span className="icon-bar"></span>
+            			<span className="icon-bar"></span>
+          			</button>
+         			<a className="navbar-brand" href="file:///C:/Users/danze/workspace/Web%20Dev/Web-Dev/Portfolio/index.html">Danzel Rana</a>
+        		</div>
+        		<div className="collapse navbar-collapse" id="myNavbar">
+          			<ul className="nav navbar-nav navbar-right">
+            			<li><a href="#about">About</a></li>
+            			<li><a href="#portfolio">Portfolio</a></li>
+            			<li><a href="#contact">Contact</a></li>
+          			</ul>
+        		</div>
+      		</nav>
+		);
+	}
+});
+
 var Header = React.createClass({
 	render: function() {
 		return (
 			<header>
-				<div className="row">
-					<div className="col-md-6">
-						<h1>Hey, there! My name is Danzel and I'm a software
-						engineer and aspiring web developer. I graduated from California
-						State Polytechnic University, Pomona with a Bachelor's degree in Computer Science
-						and am focusing my career on Full Stack Web Development.</h1>
-						<h2>{this.props.titles}</h2>
-					</div>
-					<div className="col-md-6">
-						<h1>TEST</h1>
-					</div>
-				</div>			
+				<div className="text-center" id="about">	
+					<div className="row">
+						<div className="col-md-6">
+							<h1>Hey, there! My name is Danzel and I'm a software
+							engineer and aspiring web developer. I graduated from California
+							State Polytechnic University, Pomona with a Bachelor's degree in Computer Science
+							and am focusing my career on Full Stack Web Development.</h1>
+							<h2>{this.props.titles}</h2>
+						</div>
+						<div className="col-md-6">
+							<h1>TEST</h1>
+						</div>
+					</div>	
+				</div>		
 			</header>
 		);
 	}
@@ -23,7 +49,8 @@ var Main = React.createClass({
 	render: function() {
 		return (
 			<main>
-				<h3>test main</h3>
+				<div className="text-center" id="portfolio">
+				</div>
 			</main>
 		);
 	}
@@ -32,7 +59,8 @@ var Main = React.createClass({
 var Content = React.createClass({
 	render: function() {
 		return (
-			<div>			
+			<div>
+				<Nav />	
 				<Header titles="Web Developer - Software Engineer"/>
 				<Main />
 			</div>
