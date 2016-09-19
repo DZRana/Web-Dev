e3a69bbc1e2112b0e06a9d6d8bd4da0b6c71b83b
DZRana <danzelrana@gmail.com>
2016-09-18 22:06:21 -0700

    API integration

--- TwitchChromeExtension/scripts.js
+++ TwitchChromeExtension/scripts.js
@@ -1,14 +1,40 @@
+/*
+ TODO: 1. toggle button on/off
+       2. keep track of streamers you already notified user about
+       3. link in notification
+*/
+var streamerURL = "";
+var streamerName = "";
+var streamerApiURL = "";
+var streamerStatus = "";
+var onlineArr= [];
 var opt = {
   type: "basic",
-  iconUrl: "twitch_online.png",
-  title: "Test Title",
-  message: "Test Message"
+  iconUrl: null,
+  title: null,
+  message: null
 };
 
-chrome.notifications.create(opt, callback);
-
-console.log ("Popup Done?");
-
-function callback() {
-  console.log("Popup Done!");
+function callAPI() {
+  $.getJSON("https://api.twitch.tv/kraken/users/toad555/follows/channels?client_id=7ebtgqdiu10i5ae2m6vji0waf2y4219", function(data) {
+    console.log(data);
+    for (var i = 0; i < (data.follows).length; i++) {
+      streamerURL = data.follows[i].channel.url;
+      streamerName = streamerURL.substr(22);
+      streamerApiURL = "https://api.twitch.tv/kraken/streams/" + streamerName + "?client_id=7ebtgqdiu10i5ae2m6vji0waf2y4219";
+      $.getJSON(streamerApiURL, function(streamerData) {    
+        streamerName = streamerData._links.channel.substr(38);
+        streamerURL = "https://www.twitch.tv/" + streamerName;
+        if (streamerData.stream !== null) {
+          opt.iconUrl = streamerData.stream.channel.logo;
+          opt.title = streamerName;
+          opt.message = streamerData.stream.game + ": " + streamerData.stream.channel.status;
+          chrome.notifications.create(opt);
+        }
+      }); 
+    }
+  });
 }
+
+callAPI();
+window.setInterval(callAPI, 10000);
