67d9f1725f2256289ff3a29b6c8211ed963ce991
DZRana <danzelrana@gmail.com>
2016-10-13 15:19:15 -0700

    portfolio with react components

---
+++ reactportfolio/app/components/Board.js
@@ -0,0 +1,11 @@
+var React = require("react");
+
+var Board = React.createClass({
+  render: function() {
+    return (
+      <div>fawdawd</div>
+    )
+  }
+});
+
+module.exports = Board;
---
+++ reactportfolio/app/index.js
@@ -0,0 +1,8 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+var Board = require("./components/Board");
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById("sticky-notes")
+);
