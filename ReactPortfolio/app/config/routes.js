60943656c176c13e4813bbd537e0903a953a2b0f
DZRana <danzelrana@gmail.com>
2017-02-03 11:53:24 -0800

    configured routing

--- ReactPortfolio/app/config/routes.js
+++ ReactPortfolio/app/config/routes.js
@@ -1,16 +1,19 @@
 var React = require("react");
-var ReactRouter = require("react-router");
-var Router = ReactRouter.Router;
-var Route = ReactRouter.Route;
-var IndexRoute = ReactRouter.IndexRoute;
-var hashHistory = ReactRouter.hashHistory;
+var Router = require("react-router").Router;
+var Route = require("react-router").Route;
+var IndexRoute = require("react-router").IndexRoute;
+var hashHistory = require("react-router").hashHistory;
 var Main = require("../components/Main");
 var Home = require("../components/Home");
+var About = require("../components/About");
+var Portfolio = require("../components/Portfolio");
 
 var routes = (
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
       <IndexRoute component={Home} />
+      <Route path="about" component={About} />
+      <Route path="portfolio" component={Portfolio} />
     </Route>
   </Router>
 )
--- ReactPortfolio/app/index.js
+++ ReactPortfolio/app/index.js
@@ -1,5 +1,4 @@
 var React = require("react");
-var ReactRouter = require("react-router");
 var ReactDOM = require("react-dom");
 var routes = require("./config/routes.js");
 
